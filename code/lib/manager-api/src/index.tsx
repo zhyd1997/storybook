2e4ffd4bd6836cbb8972a942e35337be80450720
Tom Coleman <tom@chromatic.com>
2024-03-27 14:31:29 +1100

    Add `userGlobals` to manager globals code

--- code/lib/manager-api/src/index.tsx
+++ code/lib/manager-api/src/index.tsx
@@ -28,6 +28,7 @@ import type {
   API_StoryEntry,
   Parameters,
   StoryId,
+  Globals,
 } from '@storybook/types';
 
 import {
@@ -498,9 +499,9 @@ export function useArgs(): [Args, (newArgs: Args) => void, (argNames?: string[])
   return [args!, updateArgs, resetArgs, initialArgs!];
 }
 
-export function useGlobals(): [Args, (newGlobals: Args) => void] {
+export function useGlobals(): [Globals, (newGlobals: Globals) => void, Globals] {
   const api = useStorybookApi();
-  return [api.getGlobals(), api.updateGlobals];
+  return [api.getGlobals(), api.updateGlobals, api.getUserGlobals()];
 }
 
 export function useGlobalTypes(): ArgTypes {
