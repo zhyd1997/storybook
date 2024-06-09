304f13e12123618521ac8b448fa3db5f53999e93
Lars Rickert <dev@lars-rickert.de>
2024-06-09 13:55:04 +0200

    feat: support script setup and v-models

--- code/renderers/vue3/src/docs/sourceDecorator.test.ts
+++ code/renderers/vue3/src/docs/sourceDecorator.test.ts
@@ -1,13 +1,18 @@
 import { expect, test } from 'vitest';
 import { h } from 'vue';
+import type { SourceCodeGeneratorContext } from './sourceDecorator';
 import {
-  extractSlotNames,
   generatePropsSourceCode,
   generateSlotSourceCode,
+  generateSourceCode,
+  parseDocgenInfo,
 } from './sourceDecorator';
 
 test('should generate source code for props', () => {
-  const slots = ['default', 'testSlot'];
+  const ctx: SourceCodeGeneratorContext = {
+    scriptVariables: {},
+    imports: {},
+  };
 
   const code = generatePropsSourceCode(
     {
@@ -19,7 +24,7 @@ test('should generate source code for props', () => {
       f: [1, 2, 3],
       g: {
         g1: 'foo',
-        b2: 42,
+        g2: 42,
       },
       h: undefined,
       i: null,
@@ -27,15 +32,28 @@ test('should generate source code for props', () => {
       k: BigInt(9007199254740991),
       l: Symbol(),
       m: Symbol('foo'),
+      modelValue: 'test-v-model',
+      otherModelValue: 42,
       default: 'default slot',
       testSlot: 'test slot',
     },
-    slots
+    ['default', 'testSlot'],
+    ['update:modelValue', 'update:otherModelValue'],
+    ctx
   );
 
   expect(code).toBe(
-    `a="foo" b='"I am double quoted"' :c="42" d :e="false" :f="[1,2,3]" :g="{'g1':'foo','b2':42}" :k="BigInt(9007199254740991)" :l="Symbol()" :m="Symbol('foo')"`
+    `a="foo" b='"I am double quoted"' :c="42" d :e="false" :f="f" :g="g" :k="BigInt(9007199254740991)" :l="Symbol()" :m="Symbol('foo')" v-model="modelValue" v-model:otherModelValue="otherModelValue"`
   );
+
+  expect(ctx.scriptVariables).toStrictEqual({
+    f: `[1,2,3]`,
+    g: `{"g1":"foo","g2":42}`,
+    modelValue: 'ref("test-v-model")',
+    otherModelValue: 'ref(42)',
+  });
+
+  expect(Array.from(ctx.imports.vue.values())).toStrictEqual(['ref']);
 });
 
 test('should generate source code for slots', () => {
@@ -106,7 +124,10 @@ child 2</template>
 
 <template #m>{{ BigInt(9007199254740991) }}</template>`;
 
-  let actualCode = generateSlotSourceCode(slots, Object.keys(slots));
+  let actualCode = generateSlotSourceCode(slots, Object.keys(slots), {
+    scriptVariables: {},
+    imports: {},
+  });
   expect(actualCode).toBe(expectedCode);
 
   // should generate the same code if getters/functions are used to return the slot content
@@ -117,7 +138,10 @@ child 2</template>
     return obj;
   }, {});
 
-  actualCode = generateSlotSourceCode(slotsWithGetters, Object.keys(slotsWithGetters));
+  actualCode = generateSlotSourceCode(slotsWithGetters, Object.keys(slotsWithGetters), {
+    scriptVariables: {},
+    imports: {},
+  });
   expect(actualCode).toBe(expectedCode);
 });
 
@@ -136,8 +160,46 @@ test('should generate source code for slots with bindings', () => {
 
 <template #b="{ foo }"><a :href="foo" :target="foo">Test link: {{ foo }}</a></template>`;
 
-  const actualCode = generateSlotSourceCode(slots, Object.keys(slots));
+  const actualCode = generateSlotSourceCode(slots, Object.keys(slots), {
+    imports: {},
+    scriptVariables: {},
+  });
   expect(actualCode).toBe(expectedCode);
+});
+
+test('should generate source code with <script setup> block', () => {
+  const actualCode = generateSourceCode({
+    title: 'MyComponent',
+    component: {
+      __docgenInfo: {
+        slots: [{ name: 'mySlot' }],
+        events: [{ name: 'update:c' }],
+      },
+    },
+    args: {
+      a: 42,
+      b: 'foo',
+      c: [1, 2, 3],
+      d: { bar: 'baz' },
+      mySlot: () => h('div', { test: [1, 2], d: { nestedProp: 'foo' } }),
+    },
+  });
+
+  expect(actualCode).toBe(`<script lang="ts" setup>
+import { ref } from "vue";
+
+const c = ref([1,2,3]);
+
+const d = {"bar":"baz"};
+
+const d1 = {"nestedProp":"foo"};
+
+const test = [1,2];
+</script>
+
+<template>
+  <MyComponent :a="42" b="foo" v-model:c="c" :d="d"> <template #mySlot><div :d="d1" :test="test" /></template> </MyComponent>
+</template>`);
 });
 
 test.each([
@@ -149,7 +211,21 @@ test.each([
     __docgenInfo: { slots: [{ name: 'slot-1' }, { name: 'slot-2' }, { notName: 'slot-3' }] },
     slotNames: ['slot-1', 'slot-2'],
   },
-])('should extract slots names from __docgenInfo', ({ __docgenInfo, slotNames }) => {
-  const actualNames = extractSlotNames({ __docgenInfo });
-  expect(actualNames).toStrictEqual(slotNames);
+])('should parse slots names from __docgenInfo', ({ __docgenInfo, slotNames }) => {
+  const docgenInfo = parseDocgenInfo({ __docgenInfo });
+  expect(docgenInfo.slotNames).toStrictEqual(slotNames);
+});
+
+test.each([
+  { __docgenInfo: 'invalid-value', eventNames: [] },
+  { __docgenInfo: {}, eventNames: [] },
+  { __docgenInfo: { events: 'invalid-value' }, eventNames: [] },
+  { __docgenInfo: { events: ['invalid-value'] }, eventNames: [] },
+  {
+    __docgenInfo: { events: [{ name: 'event-1' }, { name: 'event-2' }, { notName: 'event-3' }] },
+    eventNames: ['event-1', 'event-2'],
+  },
+])('should parse event names from __docgenInfo', ({ __docgenInfo, eventNames }) => {
+  const docgenInfo = parseDocgenInfo({ __docgenInfo });
+  expect(docgenInfo.eventNames).toStrictEqual(eventNames);
 });
---
+++ code/renderers/vue3/template/stories_vue3-vite-default-ts/SourceCode.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  foo: string;
+  bar?: number;
+  array: string[];
+  object: Record<string, unknown>;
+  modelValue?: string;
+}>();
+
+const emit = defineEmits<{
+  'update:modelValue': [value: string];
+}>();
+
+defineSlots<{
+  default(): any;
+  namedSlots(props: { foo: string }): any;
+}>();
+</script>
+
+<template>
+  <div>
+    <pre>{{ props }}</pre>
+
+    <slot></slot>
+    <slot name="namedSlots" foo="Hello World"></slot>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
