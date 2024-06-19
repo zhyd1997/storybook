ac31f4b14e890d9f59eaaba26879f91793496aed
Norbert de Langen <ndelangen@me.com>
2024-06-19 11:40:39 +0200

    get rid of the last bits referencing core/dist

--- code/core/src/manager/globals/runtime.ts
+++ code/core/src/manager/globals/runtime.ts
@@ -2,18 +2,18 @@ import * as REACT from 'react';
 import * as REACT_DOM from 'react-dom';
 import * as REACT_DOM_CLIENT from 'react-dom/client';
 
-import * as COMPONENTS from '@storybook/core/dist/components';
+import * as COMPONENTS from '@storybook/core/components';
 import * as ICONS from '@storybook/icons';
-import * as MANAGER_API from '@storybook/core/dist/manager-api';
+import * as MANAGER_API from '@storybook/core/manager-api';
 
-import * as CHANNELS from '@storybook/core/dist/channels';
-import * as EVENTS from '@storybook/core/dist/core-events';
-import * as EVENTS_MANAGER_ERRORS from '@storybook/core/dist/manager-errors';
-import * as ROUTER from '@storybook/core/dist/router';
-import * as THEMING from '@storybook/core/dist/theming';
-import * as THEMINGCREATE from '@storybook/core/dist/theming/create';
-import * as TYPES from '@storybook/core/dist/types';
-import * as CLIENT_LOGGER from '@storybook/core/dist/client-logger';
+import * as CHANNELS from '@storybook/core/channels';
+import * as EVENTS from '@storybook/core/core-events';
+import * as EVENTS_MANAGER_ERRORS from '@storybook/core/manager-errors';
+import * as ROUTER from '@storybook/core/router';
+import * as THEMING from '@storybook/core/theming';
+import * as THEMINGCREATE from '@storybook/core/theming/create';
+import * as TYPES from '@storybook/core/types';
+import * as CLIENT_LOGGER from '@storybook/core/client-logger';
 
 import type { globalsNameReferenceMap } from './globals';
 
@@ -26,40 +26,30 @@ export const globalsNameValueMap: Required<Record<keyof typeof globalsNameRefere
 
   '@storybook/components': COMPONENTS,
   '@storybook/core/components': COMPONENTS,
-  '@storybook/core/dist/components': COMPONENTS,
 
   '@storybook/manager-api': MANAGER_API,
   '@storybook/core/manager-api': MANAGER_API,
-  '@storybook/core/dist/manager-api': MANAGER_API,
 
   '@storybook/router': ROUTER,
   '@storybook/core/router': ROUTER,
-  '@storybook/core/dist/router': ROUTER,
 
   '@storybook/theming': THEMING,
   '@storybook/core/theming': THEMING,
-  '@storybook/core/dist/theming': THEMING,
   '@storybook/theming/create': THEMINGCREATE,
   '@storybook/core/theming/create': THEMINGCREATE,
-  '@storybook/core/dist/theming/create': THEMINGCREATE,
 
   '@storybook/channels': CHANNELS,
   '@storybook/core/channels': CHANNELS,
-  '@storybook/core/dist/channels': CHANNELS,
 
   '@storybook/core-events': EVENTS,
   '@storybook/core/core-events': EVENTS,
-  '@storybook/core/dist/core-events': EVENTS,
 
   '@storybook/types': TYPES,
   '@storybook/core/types': TYPES,
-  '@storybook/core/dist/types': TYPES,
 
   '@storybook/core-events/manager-errors': EVENTS_MANAGER_ERRORS,
   '@storybook/core/manager-errors': EVENTS_MANAGER_ERRORS,
-  '@storybook/core/dist/manager-errors': EVENTS_MANAGER_ERRORS,
 
   '@storybook/client-logger': CLIENT_LOGGER,
   '@storybook/core/client-logger': CLIENT_LOGGER,
-  '@storybook/core/dist/client-logger': CLIENT_LOGGER,
 };
--- code/core/src/preview/globals/runtime.ts
+++ code/core/src/preview/globals/runtime.ts
@@ -1,11 +1,11 @@
 import * as GLOBAL from '@storybook/global';
 
-import * as CHANNELS from '@storybook/core/dist/channels';
-import * as CLIENT_LOGGER from '@storybook/core/dist/client-logger';
-import * as CORE_EVENTS from '@storybook/core/dist/core-events';
-import * as CORE_EVENTS_PREVIEW_ERRORS from '@storybook/core/dist/preview-errors';
-import * as PREVIEW_API from '@storybook/core/dist/preview-api';
-import * as TYPES from '@storybook/core/dist/types';
+import * as CHANNELS from '@storybook/core/channels';
+import * as CLIENT_LOGGER from '@storybook/core/client-logger';
+import * as CORE_EVENTS from '@storybook/core/core-events';
+import * as CORE_EVENTS_PREVIEW_ERRORS from '@storybook/core/preview-errors';
+import * as PREVIEW_API from '@storybook/core/preview-api';
+import * as TYPES from '@storybook/core/types';
 
 import type { globalsNameReferenceMap } from './globals';
 
@@ -15,25 +15,19 @@ export const globalsNameValueMap: Required<Record<keyof typeof globalsNameRefere
 
   '@storybook/channels': CHANNELS,
   '@storybook/core/channels': CHANNELS,
-  '@storybook/core/dist/channels': CHANNELS,
 
   '@storybook/client-logger': CLIENT_LOGGER,
   '@storybook/core/client-logger': CLIENT_LOGGER,
-  '@storybook/core/dist/client-logger': CLIENT_LOGGER,
 
   '@storybook/core-events': CORE_EVENTS,
   '@storybook/core/core-events': CORE_EVENTS,
-  '@storybook/core/dist/core-events': CORE_EVENTS,
 
   '@storybook/core-events/preview-errors': CORE_EVENTS_PREVIEW_ERRORS,
   '@storybook/core/preview-errors': CORE_EVENTS_PREVIEW_ERRORS,
-  '@storybook/core/dist/preview-errors': CORE_EVENTS_PREVIEW_ERRORS,
 
   '@storybook/preview-api': PREVIEW_API,
   '@storybook/core/preview-api': PREVIEW_API,
-  '@storybook/core/dist/preview-api': PREVIEW_API,
 
   '@storybook/types': TYPES,
   '@storybook/core/types': TYPES,
-  '@storybook/core/dist/types': TYPES,
 };
