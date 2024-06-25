aa6684377239e6e60b94df98ee5d6c8be47dde25
Jeppe Reinhold <jeppe@reinhold.is>
2024-06-25 20:59:01 +0200

    Remove unused "Blocks Only" Storybook

--- code/.storybook/main.ts
+++ code/.storybook/main.ts
@@ -3,67 +3,40 @@ import path from 'path';
 import { mergeConfig } from 'vite';
 import type { StorybookConfig } from '../frameworks/react-vite';
 
-const isBlocksOnly = process.env.STORYBOOK_BLOCKS_ONLY === 'true';
-
-const allStories = [
-  {
-    directory: '../core/src/manager',
-    titlePrefix: '@manager',
-  },
-  {
-    directory: '../core/src/preview-api',
-    titlePrefix: '@preview',
-  },
-  {
-    directory: '../core/src/components',
-    titlePrefix: '@components',
-  },
-  {
-    directory: '../lib/blocks/src',
-    titlePrefix: '@blocks',
-  },
-  {
-    directory: '../addons/controls/src', // TODO other addons?
-    titlePrefix: '@addons/controls',
-  },
-  {
-    directory: '../addons/onboarding/src',
-    titlePrefix: '@addons/onboarding',
-  },
-  {
-    directory: '../addons/interactions/src',
-    titlePrefix: '@addons/interactions',
-  },
-];
-
-/**
- * match all stories in blocks/src/blocks, blocks/src/controls and blocks/src/examples EXCEPT blocks/src/blocks/internal
- * Examples:
- *
- * src/blocks/Canvas.stories.tsx - MATCH
- * src/blocks/internal/InternalCanvas.stories.tsx - IGNORED, internal stories
- * src/blocks/internal/nested/InternalCanvas.stories.tsx - IGNORED, internal stories
- *
- * src/blocks/Canvas.tsx - IGNORED, not story
- * src/blocks/nested/Canvas.stories.tsx - MATCH
- * src/blocks/nested/deep/Canvas.stories.tsx - MATCH
- *
- * src/controls/Boolean.stories.tsx - MATCH
- * src/controls/Boolean.tsx - IGNORED, not story
- *
- * src/components/ColorPalette.stories.tsx - MATCH
- * src/components/ColorPalette.tsx - IGNORED, not story
- */
-const blocksOnlyStories = [
-  '../blocks/src/@(blocks|controls|examples)/!(internal)/**/*.@(mdx|stories.@(js|jsx|mjs|ts|tsx))',
-  '../blocks/src/@(blocks|controls|examples)/*.@(mdx|stories.@(js|jsx|mjs|ts|tsx))',
-];
-
 const componentsPath = path.join(__dirname, '../core/src/components');
 const managerApiPath = path.join(__dirname, '../core/src/manager-api');
 
 const config: StorybookConfig = {
-  stories: isBlocksOnly ? blocksOnlyStories : allStories,
+  stories: [
+    {
+      directory: '../core/src/manager',
+      titlePrefix: '@manager',
+    },
+    {
+      directory: '../core/src/preview-api',
+      titlePrefix: '@preview',
+    },
+    {
+      directory: '../core/src/components',
+      titlePrefix: '@components',
+    },
+    {
+      directory: '../lib/blocks/src',
+      titlePrefix: '@blocks',
+    },
+    {
+      directory: '../addons/controls/src',
+      titlePrefix: '@addons/controls',
+    },
+    {
+      directory: '../addons/onboarding/src',
+      titlePrefix: '@addons/onboarding',
+    },
+    {
+      directory: '../addons/interactions/src',
+      titlePrefix: '@addons/interactions',
+    },
+  ],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
