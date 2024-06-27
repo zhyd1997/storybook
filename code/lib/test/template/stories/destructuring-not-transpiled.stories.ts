732efd723af0283db468a1153c8e48371f887419
Kasper Peulen <kasperpeulen@gmail.com>
2024-06-27 14:21:43 +0200

    Only test in the test runner

--- code/lib/test/template/stories/destructuring-not-transpiled.stories.ts
+++ code/lib/test/template/stories/destructuring-not-transpiled.stories.ts
@@ -9,6 +9,7 @@ export default {
 // We must not transpile destructuring, to make sure that we can analyze the context properties that are used in play.
 // See: https://github.com/storybookjs/storybook/discussions/27389
 export const DestructureNotTranspiled = {
+  parameters: { chromatic: { disable: true } },
   async play() {
     async function fn({ destructured }: { destructured: unknown }) {
       console.log(destructured);
