b9f4f0401bcc479b27d3250d483e54c7a9a78c31
Kasper Peulen <kasperpeulen@gmail.com>
2024-06-28 14:07:54 +0200

    Fix test type

--- code/lib/preview-api/src/modules/store/csf/prepareStory.test.ts
+++ code/lib/preview-api/src/modules/store/csf/prepareStory.test.ts
@@ -49,7 +49,7 @@ export function prepareStory<TRenderer extends Renderer>(
 const addExtraContext = (
   context: PreparedStory & Pick<StoryContext, 'args' | 'globals'>
 ): StoryContext => {
-  const extraContext = {
+  const extraContext: StoryContext = {
     ...context,
     hooks: new HooksContext(),
     viewMode: 'story' as const,
@@ -57,7 +57,8 @@ const addExtraContext = (
     abortSignal: new AbortController().signal,
     canvasElement: {},
     step: vi.fn(),
-    context: undefined! as StoryContext,
+    context: null! as StoryContext,
+    canvas: null!,
   };
   extraContext.context = extraContext;
   return extraContext;
