8888f6435f48b6bf2666b081778ea792b0e0f227
Kasper Peulen <kasperpeulen@gmail.com>
2024-07-05 23:14:58 +0200

    Fix wrong refactor

--- code/core/src/preview-errors.ts
+++ code/core/src/preview-errors.ts
@@ -240,7 +240,7 @@ export class MountMustBeDestructuredError extends StorybookError {
 }
 
 export class TestingLibraryMustBeConfiguredError extends StorybookError {
-  constructor(public data: { importType: string }) {
+  constructor() {
     super({
       category: Category.PREVIEW_API,
       code: 13,
