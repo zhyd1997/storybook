1adbd1cb8d96c2617f145766cb85ce2e9144481c
Norbert de Langen <ndelangen@me.com>
2024-07-26 08:50:13 +0200

    disable webkit in e2e tests for now,due to flake:
    https://app.circleci.com/pipelines/github/storybookjs/storybook/81120/workflows/16a6a303-ed49-45c8-be74-769ff0cf1253/jobs/686641/artifacts

--- code/playwright.config.ts
+++ code/playwright.config.ts
@@ -67,12 +67,12 @@ export default defineConfig({
       },
     },
 
-    {
-      name: 'webkit',
-      use: {
-        ...devices['Desktop Safari'],
-      },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: {
+    //     ...devices['Desktop Safari'],
+    //   },
+    // },
 
     /* Test against mobile viewports. */
     // {
