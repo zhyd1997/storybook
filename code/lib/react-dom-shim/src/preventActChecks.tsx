8f39a197d3aece3e5d096445a6bc0778d3307771
Kasper Peulen <kasperpeulen@gmail.com>
2024-08-05 11:28:03 +0200

    Remove write issue reminder

--- code/lib/react-dom-shim/src/preventActChecks.tsx
+++ code/lib/react-dom-shim/src/preventActChecks.tsx
@@ -6,7 +6,6 @@ declare const globalThis: {
 
 // TODO(9.0): We should actually wrap all those lines in `act`, but that might be a breaking change.
 // We should make that breaking change for SB 9.0
-// Write issue
 export function preventActChecks(callback: () => void): void {
   const originalActEnvironment = globalThis.IS_REACT_ACT_ENVIRONMENT;
   globalThis.IS_REACT_ACT_ENVIRONMENT = false;
