a25f79a6825be25ff3c78e8d28ebb86ca9fd4d8d
Valentin Palkovic <valentin@chromatic.com>
2024-08-05 11:44:43 +0200

    Cleanup

--- scripts/prepare/check.ts
+++ scripts/prepare/check.ts
@@ -39,10 +39,7 @@ run({ cwd: process.cwd() }).catch((err: unknown) => {
 
 function getTSDiagnostics(program: ts.Program, cwd: string, host: ts.CompilerHost): any {
   return ts.formatDiagnosticsWithColorAndContext(
-    ts.getPreEmitDiagnostics(program).filter((d) => {
-      // console.log(d);
-      return d.file.fileName.startsWith(cwd);
-    }),
+    ts.getPreEmitDiagnostics(program).filter((d) => d.file.fileName.startsWith(cwd)),
     host
   );
 }
