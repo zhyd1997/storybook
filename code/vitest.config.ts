fb56f61c7e381b9fc8f84d1a0be1ccc76f10e8ee
Kasper Peulen <kasperpeulen@gmail.com>
2024-08-06 12:53:48 +0200

    Fix check errors

--- code/vitest.config.ts
+++ code/vitest.config.ts
@@ -3,7 +3,14 @@ import { coverageConfigDefaults, defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     coverage: {
-      exclude: [...coverageConfigDefaults.exclude, '**/dist/**', ''],
+      exclude: [
+        ...coverageConfigDefaults.exclude,
+        '**/__mocks/**',
+        '**/dist/**',
+        'playwright.config.ts',
+        'vitest-setup.ts',
+        'vitest.helpers.ts',
+      ],
     },
   },
 });
