72164de4dd2b191eb3638c44a50a608957295fbc
Yann Braga <yannbf@gmail.com>
2024-08-09 15:41:45 +0200

    add browser interactivity api experiment

--- code/.storybook/storybook.setup.ts
+++ code/.storybook/storybook.setup.ts
@@ -1,4 +1,4 @@
-import { vi, beforeAll, beforeEach } from 'vitest';
+import { vi, beforeAll, expect as vitestExpect } from 'vitest';
 import { setProjectAnnotations } from '@storybook/react';
 import * as projectAnnotations from './preview';
 import * as componentAnnotations from '../core/template/stories/preview';
@@ -6,17 +6,32 @@ import * as coreAnnotations from '../addons/toolbars/template/stories/preview';
 // register global components used in many stories
 import '../renderers/react/template/components';
 
-const { cleanup, render: testingLibraryRender } = await import('@testing-library/react/pure');
-
-beforeEach(cleanup);
+import { userEvent as storybookEvent, expect as storybookExpect } from '@storybook/test';
 
 vi.spyOn(console, 'warn').mockImplementation((...args) => console.log(...args));
 
 const annotations = setProjectAnnotations([
+  // @ts-expect-error check type errors later
   projectAnnotations,
+  // @ts-expect-error check type errors later
   componentAnnotations,
   coreAnnotations,
-  { testingLibraryRender },
+  {
+    // experiment with injecting Vitest's interactivity API over our userEvent while tests run in browser mode
+    // https://vitest.dev/guide/browser/interactivity-api.html
+    loaders: async (context) => {
+      // eslint-disable-next-line no-underscore-dangle
+      if (globalThis.__vitest_browser__) {
+        const vitest = await import('@vitest/browser/context');
+        const { userEvent: browserEvent } = vitest;
+        context.userEvent = browserEvent.setup();
+        context.expect = vitestExpect;
+      } else {
+        context.userEvent = storybookEvent.setup();
+        context.expect = storybookExpect;
+      }
+    },
+  },
 ]);
 
 beforeAll(annotations.beforeAll!);
