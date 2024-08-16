0b6f53d9afc3235560239a733c69bef8abbfd760
Yann Braga <yannbf@gmail.com>
2024-08-16 12:45:04 +0200

    Web Components: Introduce setProjectAnnotations API

--- code/renderers/web-components/src/index.ts
+++ code/renderers/web-components/src/index.ts
@@ -7,6 +7,7 @@ const { window, EventSource } = global;
 
 export * from './public-types';
 export * from './framework-api';
+export * from './portable-stories';
 
 // TODO: disable HMR and do full page loads because of customElements.define
 if (typeof module !== 'undefined' && module?.hot?.decline) {
