fb2455bfbab954d570e165c50f6a79eb7e16dd37
ziebam <ziebamichal@tutanota.com>
2024-09-14 19:31:03 +0200

    core: Remove `fs-extra` from `src/core-server/utils`

--- code/core/src/core-server/utils/copy-all-static-files.ts
+++ code/core/src/core-server/utils/copy-all-static-files.ts
@@ -1,3 +1,4 @@
+import { cp } from 'node:fs/promises';
 import { join, relative } from 'node:path';
 
 import { getDirectoryFromWorkingDir } from '@storybook/core/common';
@@ -5,7 +6,6 @@ import { getDirectoryFromWorkingDir } from '@storybook/core/common';
 import { logger } from '@storybook/core/node-logger';
 
 import chalk from 'chalk';
-import fs from 'fs-extra';
 
 import { parseStaticDir } from './server-statics';
 
@@ -26,10 +26,13 @@ export async function copyAllStaticFiles(staticDirs: any[] | undefined, outputDi
 
           // Storybook's own files should not be overwritten, so we skip such files if we find them
           const skipPaths = ['index.html', 'iframe.html'].map((f) => join(targetPath, f));
-          await fs.copy(staticPath, targetPath, {
+          // TODO: `fsPromises.cp` is marked as experimental in Node 16-21. Ask in the PR whether we should
+          // use it anyway or stick to `fs-extra` for now.
+          await cp(staticPath, targetPath, {
             dereference: true,
             preserveTimestamps: true,
             filter: (_, dest) => !skipPaths.includes(dest),
+            recursive: true,
           });
         } catch (e) {
           if (e instanceof Error) {
@@ -68,10 +71,11 @@ export async function copyAllStaticFilesRelativeToMain(
         `=> Copying static files: ${chalk.cyan(print(from))} at ${chalk.cyan(print(targetPath))}`
       );
     }
-    await fs.copy(from, targetPath, {
+    await cp(from, targetPath, {
       dereference: true,
       preserveTimestamps: true,
       filter: (_, dest) => !skipPaths.includes(dest),
+      recursive: true,
     });
   }, Promise.resolve());
 }
