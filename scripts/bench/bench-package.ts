03dea27c8077b0a9a6baf575bb7f6fa913d0ace9
Jeppe Reinhold <jeppe@reinhold.is>
2024-10-11 22:53:30 +0200

    initial script to bench individual packages

---
+++ scripts/bench/bench-package.ts
@@ -0,0 +1,157 @@
+import { program } from 'commander';
+import detectFreePort from 'detect-port';
+import { mkdir, readdir, rm, stat, writeFile } from 'fs/promises';
+import pLimit from 'p-limit';
+import { join } from 'path';
+import { x } from 'tinyexec';
+
+import versions from '../../code/core/src/common/versions';
+import { runRegistry } from '../tasks/run-registry';
+import { maxConcurrentTasks } from '../utils/concurrency';
+import { esMain } from '../utils/esmain';
+
+const PACKAGES_PATH = join(__dirname, '..', '..', 'bench', 'packages');
+const REGISTRY_PORT = 6001;
+
+type PackageName = keyof typeof versions;
+
+/**
+ * This script is used to bench the size of Storybook packages and their dependencies. For each
+ * package, the steps are:
+ *
+ * 1. Create a temporary directory in /bench/packages and create a package.json file that only depends
+ *    on that one package
+ * 2. Install the package and its dependencies, without peer dependencies
+ * 3. Measure the size of the package and its dependencies, and count the number of dependencies
+ *    (including transitive)
+ * 4. Print the results
+ */
+
+export const benchPackage = async (packageName: PackageName) => {
+  console.log(`Benching ${packageName}...`);
+  const packagePath = join(PACKAGES_PATH, packageName.replace('@storybook', ''));
+
+  await rm(packagePath, { recursive: true }).catch(() => {});
+  await mkdir(packagePath, { recursive: true });
+
+  await writeFile(
+    join(packagePath, 'package.json'),
+    JSON.stringify(
+      {
+        name: `${packageName}-bench`,
+        version: '1.0.0',
+        overrides: versions,
+      },
+      null,
+      2
+    )
+  );
+
+  await x(
+    'npm',
+    `install --save-exact --registry http://localhost:6001 --omit peer ${packageName}@${versions[packageName]}`.split(
+      ' '
+    ),
+    {
+      nodeOptions: { cwd: packagePath },
+    }
+  );
+
+  const npmLsResult = await x('npm', `ls --all --parseable`.split(' '), {
+    nodeOptions: { cwd: packagePath },
+  });
+
+  const amountOfDependencies = npmLsResult.stdout.trim().split('\n').length - 2;
+  // the first line is the temporary benching package itself, don't count it
+  // the second line is the package we're benching, don't count it
+
+  const nodeModulesSize = await getDirSize(join(packagePath, 'node_modules'));
+  const selfSize = await getDirSize(join(packagePath, 'node_modules', packageName));
+  const dependencySize = nodeModulesSize - selfSize;
+
+  console.log({
+    package: packageName,
+    dependencies: amountOfDependencies,
+    selfSize: formatBytes(selfSize),
+    dependencySize: formatBytes(dependencySize),
+    totalSize: formatBytes(nodeModulesSize),
+  });
+};
+
+const getDirSize = async (path: string) => {
+  const entities = await readdir(path, {
+    recursive: true,
+    withFileTypes: true,
+  });
+  const stats = await Promise.all(
+    entities
+      .filter((entity) => entity.isFile())
+      .map((entity) => stat(join(entity.parentPath, entity.name)))
+  );
+  return stats.reduce((acc, { size }) => acc + size, 0);
+};
+
+const formatBytes = (bytes: number) => {
+  const units = ['B', 'KB', 'MB', 'GB', 'TB'];
+  let size = bytes;
+  let unitIndex = 0;
+
+  while (size >= 1000 && unitIndex < units.length - 1) {
+    size /= 1000;
+    unitIndex++;
+  }
+
+  // show 2 decimal places for MB, GB and TB and 0 for KB, B
+  const decimals = unitIndex < 2 ? 0 : 2;
+  return `${size.toFixed(decimals)} ${units[unitIndex]}`;
+};
+
+const run = async () => {
+  program.argument(
+    '[packages...]',
+    'which packages to bench. If omitted, all packages are benched'
+  );
+  program.parse(process.argv);
+
+  const packages = (
+    program.args.length > 0 ? program.args : Object.keys(versions)
+  ) as PackageName[];
+
+  packages.forEach((packageName) => {
+    if (!Object.keys(versions).includes(packageName)) {
+      throw new Error(`Package '${packageName}' not found`);
+    }
+  });
+
+  let registryController: AbortController | undefined;
+  if ((await detectFreePort(REGISTRY_PORT)) === REGISTRY_PORT) {
+    registryController = await runRegistry({ dryRun: false, debug: false });
+  }
+
+  // The amount of VCPUs for this task in CI is 8 (xlarge resource)
+  const amountOfVCPUs = 8;
+  const concurrentLimt = process.env.CI ? amountOfVCPUs - 1 : maxConcurrentTasks;
+  const limit = pLimit(concurrentLimt);
+
+  const progressIntervalId = setInterval(() => {
+    const doneCount = packages.length - limit.activeCount - limit.pendingCount;
+    if (doneCount === packages.length) {
+      clearInterval(progressIntervalId);
+      return;
+    }
+    console.log(
+      `Currently benching ${limit.activeCount} packages, ${limit.pendingCount} pending, ${doneCount} done...`
+    );
+  }, 2_000);
+  await Promise.all(packages.map((packageName) => limit(() => benchPackage(packageName))));
+
+  console.log('Done benching all packages');
+  registryController?.abort();
+};
+
+if (esMain(import.meta.url)) {
+  run().catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
+}
