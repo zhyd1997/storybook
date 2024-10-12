2b53b6ead9f5052e6183193393b143043314dbb0
Michael Shilman <shilman@users.noreply.github.com>
2024-10-12 09:56:47 +0800

    Cleanup

--- code/core/src/core-server/presets/common-manager.ts
+++ code/core/src/core-server/presets/common-manager.ts
@@ -1,22 +1,9 @@
 import { global } from '@storybook/global';
-import type { Tag } from '@storybook/types';
 
-const TAG_FILTERS = 'tag-filters';
 import { addons } from '@storybook/core/manager-api';
 
+const TAG_FILTERS = 'tag-filters';
 const STATIC_FILTER = 'static-filter';
-
-const parseTags = (tags?: string) => {
-  if (!tags) return undefined;
-  return tags.split(',').reduce(
-    (acc, tag) => {
-      tag.trim();
-      acc[tag] = true;
-      return acc;
-    },
-    {} as Record<Tag, boolean>
-  );
-};
 
 addons.register(TAG_FILTERS, (api) => {
   // FIXME: this ensures the filter is applied after the first render
@@ -33,24 +20,6 @@ addons.register(TAG_FILTERS, (api) => {
   );
 
   api.experimental_setFilter(STATIC_FILTER, (item) => {
-    //   const tags = item.tags || [];
-    //   return tags.filter((tag) => staticExludeTags[tag]).length === 0;
-    // });
-
-    // api.experimental_setFilter(UI_FILTER, (item) => {
-    //   const tags = item.tags || [];
-    //   const { queryParams } = api.getUrlState();
-    //   const tagToInclude = parseTags(queryParams.tagsFilter);
-
-    //   if (!tagToInclude) return true;
-
-    //   let include = true;
-    //   include = tags.some((tag) => tagToInclude[tag));
-
-    //   if (excludeTags) {
-    //     include = !tags.some((tag) => excludeTags.includes(tag));
-    //   }
-    //   return include;
     const tags = item.tags ?? [];
     return (
       // we can filter out the primary story, but we still want to show autodocs
