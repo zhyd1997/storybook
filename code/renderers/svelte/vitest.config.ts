d22b8e92510f04bd9895d9b0d18b5d1a2ab1118b
Jeppe Reinhold <jeppe@reinhold.is>
2024-10-17 20:42:45 +0200

    fix types in vitest config

--- code/renderers/svelte/vitest.config.ts
+++ code/renderers/svelte/vitest.config.ts
@@ -6,6 +6,7 @@ export default defineConfig(
   mergeConfig(vitestCommonConfig, {
     plugins: [
       import('@sveltejs/vite-plugin-svelte').then(({ svelte }) => svelte()),
+      // @ts-expect-error -- types don't match our TS module resolution setting
       import('@testing-library/svelte/vite').then(({ svelteTesting }) => svelteTesting()),
     ],
     test: {
