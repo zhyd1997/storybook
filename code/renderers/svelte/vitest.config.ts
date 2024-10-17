47541e8508b74e1d00ee329e9f49f963e96229c1
Jeppe Reinhold <jeppe@reinhold.is>
2024-10-17 20:41:49 +0200

    fix Svelte vitests that broke when upgrading to most recent Svelte 5 prerelease

--- code/renderers/svelte/vitest.config.ts
+++ code/renderers/svelte/vitest.config.ts
@@ -5,9 +5,11 @@ import { vitestCommonConfig } from '../../vitest.workspace';
 export default defineConfig(
   mergeConfig(vitestCommonConfig, {
     plugins: [
-      import('@sveltejs/vite-plugin-svelte').then(({ svelte, vitePreprocess }) =>
-        svelte({ preprocess: vitePreprocess() })
-      ),
+      import('@sveltejs/vite-plugin-svelte').then(({ svelte }) => svelte()),
+      import('@testing-library/svelte/vite').then(({ svelteTesting }) => svelteTesting()),
     ],
+    test: {
+      environment: 'happy-dom',
+    },
   })
 );
