85ab53c20347f23357e65e77aa7f6fa443d3ea9c
Valentin Palkovic <valentin@chromatic.com>
2024-10-21 12:48:03 +0200

    Update playwright

--- test-storybooks/portable-stories-kitchen-sink/react/playwright-e2e.config.ts
+++ test-storybooks/portable-stories-kitchen-sink/react/playwright-e2e.config.ts
@@ -32,6 +32,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'retain-on-failure',
     video: 'retain-on-failure',
+    // headless: false,
   },
 
   /* Configure projects for major browsers */
