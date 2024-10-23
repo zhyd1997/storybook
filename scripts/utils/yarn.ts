75cc1b102171126abbd314696a8fe842ae12b81a
Kasper Peulen <kasperpeulen@gmail.com>
2024-10-23 11:05:03 +0200

    Make sure Next 14 is used for sandboxes

--- scripts/utils/yarn.ts
+++ scripts/utils/yarn.ts
@@ -116,7 +116,7 @@ export const configureYarn2ForVerdaccio = async ({
   if (
     key.includes('svelte-kit') ||
     // React prereleases will have INCOMPATIBLE_PEER_DEPENDENCY errors because of transitive dependencies not allowing v19 betas
-    key.includes('nextjs/prerelease') ||
+    key.includes('nextjs') ||
     key.includes('react-vite/prerelease') ||
     key.includes('react-webpack/prerelease')
   ) {
