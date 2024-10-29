378ab7756a849bfa40303b249bc216d326f54024
toothlessdev <toothless042@gmail.com>
2024-10-30 02:42:43 +0900

    Add init command to common CLI commands

--- code/lib/cli-storybook/src/bin/index.ts
+++ code/lib/cli-storybook/src/bin/index.ts
@@ -8,6 +8,7 @@ import { logger } from 'storybook/internal/node-logger';
 import { addToGlobalContext, telemetry } from 'storybook/internal/telemetry';
 
 import { program } from 'commander';
+import { initiate } from 'create-storybook';
 import envinfo from 'envinfo';
 import { findPackageSync } from 'fd-package-json';
 import leven from 'leven';
@@ -39,6 +40,35 @@ const command = (name: string) =>
     )
     .option('--debug', 'Get more logs in debug mode', false)
     .option('--enable-crash-reports', 'Enable sending crash reports to telemetry data');
+
+command('init')
+  .description('Initialize Storybook into your project.')
+  .option('-f --force', 'Force add Storybook')
+  .option('-s --skip-install', 'Skip installing deps')
+  .option('--package-manager <npm|pnpm|yarn1|yarn2>', 'Force package manager for installing deps')
+  .option('--use-pnp', 'Enable PnP mode for Yarn 2+')
+  .option('-p --parser <babel | babylon | flow | ts | tsx>', 'jscodeshift parser')
+  .option('-t --type <type>', 'Add Storybook for a specific project type')
+  .option('-y --yes', 'Answer yes to all prompts')
+  .option('-b --builder <webpack5 | vite>', 'Builder library')
+  .option('-l --linkable', 'Prepare installation for link (contributor helper)')
+  .option(
+    '--dev',
+    'Launch the development server after completing initialization. Enabled by default (default: true)',
+    process.env.CI !== 'true' && process.env.IN_STORYBOOK_SANDBOX !== 'true'
+  )
+  .option(
+    '--no-dev',
+    'Complete the initialization of Storybook without launching the Storybook development server'
+  )
+  .option('-V --version', 'output the version number')
+  .option('-h --help', 'display help for command')
+  .action((options) => {
+    initiate(options).catch((e) => {
+      logger.error(e);
+      process.exit(1);
+    });
+  });
 
 command('add <addon>')
   .description('Add an addon to your Storybook')
