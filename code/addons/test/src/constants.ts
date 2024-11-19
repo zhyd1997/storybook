51a4c483ecd9765703d532a24668105a849c5ae8
Gert Hengeveld <info@ghengeveld.nl>
2024-11-19 12:00:18 +0100

    Drop unused event

--- code/addons/test/src/constants.ts
+++ code/addons/test/src/constants.ts
@@ -2,7 +2,6 @@ export const ADDON_ID = 'storybook/test';
 export const TEST_PROVIDER_ID = `${ADDON_ID}/test-provider`;
 export const PANEL_ID = `${ADDON_ID}/panel`;
 export const STORYBOOK_ADDON_TEST_CHANNEL = 'STORYBOOK_ADDON_TEST_CHANNEL';
-export const RUN_TESTS_EVENT = `${ADDON_ID}/RUN_TESTS`;
 
 export const TUTORIAL_VIDEO_LINK = 'https://youtu.be/Waht9qq7AoA';
 export const DOCUMENTATION_LINK = 'writing-tests/test-addon';
