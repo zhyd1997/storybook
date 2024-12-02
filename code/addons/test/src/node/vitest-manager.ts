9360bb7ce05f34d1c77f691abef6f0efd12e7992
Norbert de Langen <ndelangen@me.com>
2024-12-02 11:54:01 +0100

    add exceptions amending the error message to prompt users to install required packages for supporting coverage

--- code/addons/test/src/node/vitest-manager.ts
+++ code/addons/test/src/node/vitest-manager.ts
@@ -83,6 +83,18 @@ export class VitestManager {
     try {
       await this.vitest.init();
     } catch (e) {
+      const isV8 = e.message?.includes('@vitest/coverage-v8');
+      const isIstanbul = e.message?.includes('@vitest/coverage-istanbul');
+
+      console.log('HELLO');
+
+      if (e.message?.includes('Failed to load url') && (isIstanbul || isV8)) {
+        const coveragePackage = isIstanbul ? 'coverage-istanbul' : 'coverage-v8';
+        e.message = `Please install the @vitest/${coveragePackage} package to run with coverage`;
+        this.testManager.reportFatalError('Failed to change coverage mode', e);
+        return;
+      }
+
       this.testManager.reportFatalError('Failed to init Vitest', e);
     }
 
