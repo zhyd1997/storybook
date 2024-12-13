bb323de1fc7413a0a7a318e76ba0286cc8c00b46
Yann Braga <yannbf@gmail.com>
2024-12-13 11:21:30 +0100

    simplify config types in experimental-nextjs-vite by reutilizing react-vite ones

--- code/frameworks/experimental-nextjs-vite/src/types.ts
+++ code/frameworks/experimental-nextjs-vite/src/types.ts
@@ -1,12 +1,7 @@
-import type {
-  CompatibleString,
-  StorybookConfig as StorybookConfigBase,
-  TypescriptOptions as TypescriptOptionsBase,
-} from 'storybook/internal/types';
+import type { CompatibleString } from 'storybook/internal/types';
 
-import type { BuilderOptions, StorybookConfigVite } from '@storybook/builder-vite';
-
-import type docgenTypescript from '@joshwooding/vite-plugin-react-docgen-typescript';
+import type { BuilderOptions } from '@storybook/builder-vite';
+import type { StorybookConfig as StorybookConfigReactVite } from '@storybook/react-vite';
 
 type FrameworkName = CompatibleString<'@storybook/experimental-nextjs-vite'>;
 type BuilderName = CompatibleString<'@storybook/builder-vite'>;
@@ -24,7 +19,7 @@ type StorybookConfigFramework = {
         name: FrameworkName;
         options: FrameworkOptions;
       };
-  core?: StorybookConfigBase['core'] & {
+  core?: StorybookConfigReactVite['core'] & {
     builder?:
       | BuilderName
       | {
@@ -34,23 +29,6 @@ type StorybookConfigFramework = {
   };
 };
 
-type TypescriptOptions = TypescriptOptionsBase & {
-  /**
-   * Sets the type of Docgen when working with React and TypeScript
-   *
-   * @default `'react-docgen'`
-   */
-  reactDocgen: 'react-docgen-typescript' | 'react-docgen' | false;
-  /** Configures `@joshwooding/vite-plugin-react-docgen-typescript` */
-  reactDocgenTypescriptOptions: Parameters<typeof docgenTypescript>[0];
-};
-
 /** The interface for Storybook configuration in `main.ts` files. */
-export type StorybookConfig = Omit<
-  StorybookConfigBase,
-  keyof StorybookConfigVite | keyof StorybookConfigFramework | 'typescript'
-> &
-  StorybookConfigVite &
-  StorybookConfigFramework & {
-    typescript?: Partial<TypescriptOptions>;
-  };
+export type StorybookConfig = Omit<StorybookConfigReactVite, keyof StorybookConfigFramework> &
+  StorybookConfigFramework;
