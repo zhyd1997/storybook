b066e5621900a4625bc3722a9af04d1d3f2f1233
Valentin Palkovic <valentin@chromatic.com>
2025-01-07 12:06:28 +0100

    refactor: change return type of renderToCanvas cleanup function to async

--- code/renderers/react/src/renderToCanvas.tsx
+++ code/renderers/react/src/renderToCanvas.tsx
@@ -112,8 +112,8 @@ export async function renderToCanvas(
     processActQueue();
   });
 
-  return () => {
-    act(() => {
+  return async () => {
+    await act(() => {
       unmountElement(canvasElement);
     });
   };
