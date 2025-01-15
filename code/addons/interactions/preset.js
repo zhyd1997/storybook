1a0d0eaa34dbe1b2628d74b5a3826b65a1d4eff0
Jeppe Reinhold <jeppe@reinhold.is>
2025-01-15 10:29:18 +0100

    move addon order check from preset.js to dist/preset.js

--- code/addons/interactions/preset.js
+++ code/addons/interactions/preset.js
@@ -1,10 +1,1 @@
-const { checkActionsLoaded } = require('./dist/preset');
-
-function previewAnnotations(entry = [], options) {
-  checkActionsLoaded(options.configDir);
-  return entry;
-}
-
-module.exports = {
-  previewAnnotations,
-};
+module.exports = require('./dist/preset');
